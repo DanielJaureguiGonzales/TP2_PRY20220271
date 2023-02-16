d16c0722eef181001793c5f43d189dae469aa220
DanielJaureguiGonzales <dank_lp123@outlook.com>
2023-02-16 01:05:42 -0500

    backend_ulcernosis_base

---
+++ ulcernosis/src/main/java/com/tp2/pry20220271/ulcernosis/UlcernosisApplication.java
@@ -0,0 +1,14 @@
+package com.tp2.pry20220271.ulcernosis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class UlcernosisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UlcernosisApplication.class, args);
+    }
+
+}
---
+++ ulcernosis/src/test/java/com/tp2/pry20220271/ulcernosis/UlcernosisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp2.pry20220271.ulcernosis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlcernosisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
